25b26651a987933622ad073437db29b029ca642c
rishab_singh <rishabh.lossfilter@gmail.com>
2024-09-01 19:46:10 +0530

    logo changed

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,20 +2,18 @@
 import { useState } from "react";
 import "../styles/navbar.css";
 import Image from "next/image";
-import logo from "../../public/rick.gif";
+import logo from "../../public/bo.png";
 export default function NavBar() {
     const [page,setPage] = useState("Home");
 
 
 
 
-    
+
     return (
       <div className="navbar">
         <div className="logo">
-            <Image src={logo} alt="Description of the image"
-        width={350} 
-        height={300}></Image>
+            <Image className="img" src={logo} alt="Description of the image"></Image>
             <span className="d3">REESAB</span>
         </div>
         <div class="nav-links">
